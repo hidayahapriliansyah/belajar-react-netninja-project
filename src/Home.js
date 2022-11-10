75bcdcb8a72849cc49d067967c3b66c15cd94fb9
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2022-11-10 09:33:48 +0700

    18 conditional loading message

--- src/Home.js
+++ src/Home.js
@@ -13,17 +13,21 @@ const Home = (props) => {
   //   const handleClickAgain = (name, e) => {
   //     console.log('Hello Again ', name, e.target.innerText);
   //   }
+  const [isPending, setIsPending] = useState(true);
 
   const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        setBlogs(data);
-      });
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          setBlogs(data);
+          setIsPending(false);
+        });
+    }, 1000);
   }, []);
 
   return (
@@ -35,6 +39,7 @@ const Home = (props) => {
       <br />
       <button onClick={handleClick}>Click Me Change Name</button>
       <button onClick={(e) => handleClickAgain('Hidayah', e)}>Click Me Again</button> */}
+      {isPending && <div>Loading ... </div>}
       {blogs && <BlogLists blogs={blogs} title="All post" />}
       {/* <BlogLists blogs={blogs.filter((blog) => blog.author === 'Adi' )} title="Adi's post" handleDelete={handleDelete}/> */}
     </section>
